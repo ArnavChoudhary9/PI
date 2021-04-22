edaf6c3766816b86f8ac7ea099315560c1139718
HotShot0901 <contact.hot.shor.1234@gmail.com>
2021-04-22 12:43:45 +0530

    Matrix Setup

--- engine/libraries/source/matrix.hpp
+++ engine/libraries/source/matrix.hpp
@@ -1,14 +1,51 @@
 #include "vectors.hpp"
 
-class Matrix {
-    public:
-        int columns, rows;
-        float **matrix;
+namespace matrix
+{
+    class Matrix {
+        public:
+            int columns, rows;
+            float **matrix;
 
-        friend std::ostream& operator << (std::ostream&, Matrix&);
-};
+            Matrix(int, int);
+            Matrix(int, int, float);
 
-std::ostream& operator << (std::ostream& dout, Matrix& m) {
-    dout << "Test";
-    return dout;
+            friend std::ostream& operator << (std::ostream&, Matrix&);
+    };
+
+    Matrix::Matrix(int _columns, int _rows) : columns{_columns}, rows{_rows} {
+        matrix = new float *[columns];
+
+        for (int x = 0; x < columns; x++) {
+            matrix[x] = new float[rows];
+
+            for (int y = 0; y < rows; y++) {
+                matrix[x][y] = 0;
+            }
+        }
+    }
+
+    Matrix::Matrix(int _columns, int _rows, float filler) : columns{_columns}, rows{_rows} {
+        matrix = new float *[columns];
+
+        for (int x = 0; x < columns; x++) {
+            matrix[x] = new float[rows];
+
+            for (int y = 0; y < rows; y++) {
+                matrix[x][y] = filler;
+            }
+        }
+    }
+
+    std::ostream& operator << (std::ostream& dout, Matrix& m) {
+        
+        for (int x = 0; x < m.columns; x++) {
+            for (int y = 0; y < m.rows; y++) {
+                dout << m.matrix[x][y] << " ";
+            }
+            dout << "\n";
+        }
+        
+        return dout;
+    }
 }
--- engine/libraries/source/renderer.cpp
+++ engine/libraries/source/renderer.cpp
@@ -2,9 +2,11 @@
 #include "matrix.hpp"
 
 using namespace std;
+using namespace vectors;
+using namespace matrix;
 
 int main() {
-    Matrix m = Matrix();
+    Matrix m = Matrix(2, 4);
 
     cout << "Hello, World!" << endl << m << endl;
 
